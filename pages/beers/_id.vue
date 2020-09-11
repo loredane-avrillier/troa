0f90e26b6e7cf158e1c35c389126b8d1d84b52cf
Loredane <loredane.avrillier@gmail.com>
2020-09-11 23:57:30 +0200

    wip

--- pages/beers/_id.vue
+++ pages/beers/_id.vue
@@ -1,51 +1,26 @@
 <template>
   <div>
-    details {{ beer }}
-    close
+    <p>{{ beer.name }}</p>
+    <p>{{ beer.description }}</p>
+    <p>{{ beer.abv }}</p>INGREDIENTS
+    <p
+      v-for="(ingredient_detail, index) in beer.ingredients"
+      :key="'ingredient_detail' + index"
+    >{{index}} {{ ingredient_detail }}</p>FOOD PAIRING
+    <p
+      v-for="(food_pairing_detail, index) in beer.food_pairing"
+      :key="'food_paring_detail' + index"
+    >{{ food_pairing_detail }}</p>
   </div>
 </template>
 <script>
 export default {
-  /*   data() {
-        return {
-            name: ""
-        }
-    }, */
-  asyncData({ $axios, params, error }) {
-    return $axios
-      .get(`https://api.punkapi.com/v2/beers/${params.id}`)
-      .then((res) => {
-        
-        return { beer:res.data }
-      })
-      .catch((e) => {
-        error({ statusCode: 404, message: "Produit non trouvé" });
-      });
+  async asyncData({ $axios, params }) {
+    let response = await $axios.get(
+      `https://api.punkapi.com/v2/beers/${params.id}`
+    );
+    console.log(response.data);
+    return { beer: response.data[0] };
   },
 };
-/* asyncData({ $axios, params, error }) {
-    return $axios
-      .get(`https://api.punkapi.com/v2/beers/${params.id}`)
-      .then(res => {
-        let data = {
-          id: res.data.data.id,
-          slug: res.data.data.slug,
-          desc: res.data.data.desc,
-          main_photo: res.data.data.main_photo,
-          weezea_ref: res.data.data.weezea_ref,
-          colors: res.data.data.colors,
-          long_desc: res.data.data.long_desc,
-          dimensions: res.data.data.dimensions,
-          material: res.data.data.material,
-          category: res.data.data.category,
-          status_id: res.data.data.status_id,
-          related: [],
-          sizes: res.data.data.sizes,
-          seo: res.data.data.seo
-        };
-       return data;
-      })
-      .catch(e => {
-        error({ statusCode: 404, message: "Produit non trouvé" });
-      }); */
 </script>
